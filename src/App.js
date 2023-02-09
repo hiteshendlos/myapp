37ad764babc84911912549704c5b5c04a9f8213a
“hiteshendlos” <hitesh.endlos@gmail.com>
2023-02-09 19:21:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 function App() {
   // State to store parsed data
   const [parsedData, setParsedData] = useState([]);
+  const [FilteredData, setFilteredData] = useState([]);
 
   //State to store table Column name
   const [tableRows, setTableRows] = useState([]);
@@ -14,11 +15,14 @@ function App() {
 
   const [hideAge, setHideAge] = useState(false);
   const [hideNationality, setHideNationality] = useState(false);
+  const [squad, setSquad] = useState(false);
+  const [minDuration, setMinDuration] = useState(0);
+  const [maxDuration, setMaxDuration] = useState(null);
 
   const changeHandler = (event) => {
     // Passing file data (event.target.files[0]) to parse using Papa.parse
     Papa.parse(event.target.files[0], {
-      // header: true,
+      header: true,
       // skipEmptyLines: true,
       complete: function (results) {
         const rowsArray = [];
@@ -48,33 +52,119 @@ function App() {
     });
   };
 
-  const newArr = [];
+  function convertArrayOfObjectsToCSV(args) {
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data || null;
+    if (data == null || !data.length) {
+      return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ",";
+    lineDelimiter = args.lineDelimiter || "\n";
+
+    keys = Object.keys(data[0]);
+
+    result = "";
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    data.forEach(function (item) {
+      ctr = 0;
+      keys.forEach(function (key) {
+        if (ctr > 0) result += columnDelimiter;
+        result += item[key];
+        ctr++;
+      });
+      result += lineDelimiter;
+    });
+
+    return result;
+  }
+
+  function downloadCSV(args) {
+    var data, filename, link;
+    var csv = convertArrayOfObjectsToCSV({
+      data: args.data,
+    });
+    if (csv == null) return;
+
+    filename = args.filename || "export.csv";
+
+    if (!csv.match(/^data:text\/csv/i)) {
+      csv = "data:text/csv;charset=utf-8," + csv;
+    }
+    data = encodeURI(csv);
+
+    link = document.createElement("a");
+    link.setAttribute("href", data);
+    link.setAttribute("download", filename);
+    link.click();
+  }
+  // const newArr = [];
+
+  const OnEveryChange = () => {
+    console.log({ maxDuration });
+    console.log({ minDuration });
+    let global = [];
+    parsedData.map((originalObject, i) => {
+      // let excludedKeys = ["Age", "Nation"];
+      let excludedKeys = [];
+
+      !hideAge && excludedKeys.push("Age");
+      !hideNationality && excludedKeys.push("Nation");
+      !squad && excludedKeys.push("Squad");
+
+      let newObject = {};
+
+      for (let key in originalObject) {
+        if (!excludedKeys.includes(key)) {
+          newObject[key] = originalObject[key];
+        }
+      }
+
+      if (
+        Number(minDuration) < newObject.Min
+
+        // &&
+        // (maxDuration > 0 || maxDuration != null
+        //   ? newObject.Min < Number(maxDuration)
+        //   : true)
+      ) {
+        // console.log(newObject.Min);
+        global.push(newObject);
+      }
+
+      // console.log(Number(minDuration));
+      // console.log(Number(maxDuration));
+
+      // if (newObject.Min > Number(minDuration) && newObject.Min < 1000) {
+      //   console.log(newObject.Min);
+      //   global.push(newObject);
+      // }
+    });
+
+    const rowsArray = [];
+    const valuesArray = [];
+
+    // Iterating data to get column name and their values
+    global.map((d) => {
+      rowsArray.push(Object.keys(d));
+      valuesArray.push(Object.values(d));
+    });
+
+    // Filtered Column Names
+    setTableRows(rowsArray[0]);
+
+    // console.log("row array", rowsArray);
+
+    // Filtered Values
+    setValues(valuesArray);
+
+    // console.log("value array", values);
+  };
+
   return (
-    // <>
-    //   <h1>Select file </h1>
-
-    //   {/* <input type={"file"} onChange={(e) => setMyfile(e.target.files[0])} /> */}
-    //   {/* <input type={"file"} onChange={(e) => setMyfile(e.target.files[0])} /> */}
-    //   <input type={"file"} onChange={changeHandler} />
-
-    //   <button
-    //     onClick={() => {
-    //       // console.log(myfile);
-    //       // parseCSVData();
-    //       // // parsingString();
-    //       // Papa.parse(CSVData, {
-    //       //   ...commonConfig,
-    //       //   complete: (result) => {
-    //       //     setParsedString(result.data);
-    //       //   },
-    //       // });
-    //       // console.log("parsingString", parsedString);
-    //     }}
-    //   >
-    //     clic
-    //   </button>
-    // </>
-
     <>
       <div>
         {/* File Uploader */}
@@ -102,7 +192,10 @@ function App() {
               type="checkbox"
               name="age"
               checked={hideAge}
-              onChange={() => setHideAge(!hideAge)}
+              onChange={() => {
+                setHideAge(!hideAge);
+                OnEveryChange();
+              }}
             />
             <br></br>
 
@@ -112,48 +205,96 @@ function App() {
               type="checkbox"
               name="Nationality"
               checked={hideNationality}
-              onChange={() => setHideNationality(!hideNationality)}
+              onChange={() => {
+                setHideNationality(!hideNationality);
+                OnEveryChange();
+              }}
             />
-          </div>
-          {/* <div className="range">
-            <p1>range filter</p1>
-
             <br></br>
-
-            <label for="age">Age</label>
+            <label for="Nationality">Squad</label>
 
             <input
-              type="range"
-              name="age"
-          
+              type="checkbox"
+              name="Nationality"
+              checked={squad}
+              onChange={() => {
+                setSquad(!squad);
+                OnEveryChange();
+              }}
             />
-            <br></br>
-          </div> */}
+          </div>
+
+          <div className="hideAndShow">
+            <p>players who have played</p>
+
+            <div>
+              Min Duration
+              <input
+                type={"number"}
+                value={minDuration}
+                onChange={(e) => {
+                  setMinDuration(e.target.value);
+
+                  console.log(e.target.value < 10);
+
+                  console.log(minDuration);
+
+                  // OnEveryChange();
+                }}
+              />
+            </div>
+            <div>
+              Maximum Duration{" "}
+              <input
+                type={"number"}
+                value={maxDuration}
+                onChange={(e) => {
+                  setMaxDuration(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              onClick={() => {
+                console.log("min", Number(minDuration));
+                console.log(
+                  "max",
+                  Number(maxDuration) < 1000 ? "true" : "myfalse"
+                );
+              }}
+            >
+              Download csv
+            </button>
+          </div>
         </div>
         <div>
           <table className="table">
             <thead>
               <tr>
-                {tableRows.map((rows, index) => {
-                  return <th key={index}>{rows}</th>;
-                })}
+                {tableRows &&
+                  tableRows.map((rows, index) => {
+                    return <th key={index}>{rows}</th>;
+                  })}
               </tr>
             </thead>
             <tbody>
-              {values.map((value, index) => {
-                if (index == 0) {
-                  return null;
-                }
-                //
+              {values &&
+                values.map((value, index) => {
+                  // if (index == 0) {
+                  //   return null;
+                  // }
+                  //
 
-                return (
-                  <tr key={index}>
-                    {value.map((val, i) => {
-                      return <td key={i}>{val}</td>;
-                    })}
-                  </tr>
-                );
-              })}
+                  return (
+                    <tr key={index}>
+                      {value.map((val, i) => {
+                        return <td key={i}>{val}</td>;
+                      })}
+                    </tr>
+                  );
+                })}
             </tbody>
           </table>
         </div>
